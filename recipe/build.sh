17f2249e9cb4e30bbad8125d6ab00b2518ba87d8
Simon Bray <32272674+simonbray@users.noreply.github.com>
2021-02-04 00:05:07 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,5 +10,12 @@ ln -s ${PREFIX}/lib/* plugins/${ARCH}/molfile
 
 export LINKER="LD_LIBRARY_PATH=plugins/${ARCH}/molfile/ ${CC}"
 
-make LINKER="${LINKER}" ARCH=${ARCH} CC=${CC} CCQHULL=${CC}
+if [[ $(uname) == "Linux" ]]; then
+    make LINKER="${LINKER}" ARCH=${ARCH} CC=${CC} CCQHULL=${CC}
+fi
+
+if [[ $(uname) == "Darwin" ]]; then
+    make fpocket LINKER="${LINKER}" ARCH=${ARCH} CC=${CC} CCQHULL=${CC}
+fi
+
 make install BINDIR=$PREFIX/bin MANDIR=$PREFIX/man/man8
