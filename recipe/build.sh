96af8a99c6b17faf817bbb9f4d3f048ebe5f65f8
Simon Bray <32272674+simonbray@users.noreply.github.com>
2021-09-07 13:58:51 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,16 @@
 if [[ $(uname) == "Linux" ]]; then
     export ARCH="LINUXAMD64"
+    CXXFLAGS=""
 fi
 
 if [[ $(uname) == "Darwin" ]]; then
     export ARCH="MACOSXX86_64"
+    CXXFLAGS="-mmacosx-version-min=10.10" 
 fi
 
-#ln -s ${PREFIX}/lib/* plugins/${ARCH}/molfile
+ln -s ${PREFIX}/lib/* plugins/${ARCH}/molfile
 
-#export LINKER="LD_LIBRARY_PATH=plugins/${ARCH}/molfile/ ${CC}"
+export LINKER="LD_LIBRARY_PATH=plugins/${ARCH}/molfile/ ${CC}"
 
-make ARCH=${ARCH} CC=${CC} CCQHULL=${CC}
+make ARCH=${ARCH} CC=${CC} CCQHULL=${CC} CXXFLAGS=${CXXFLAGS}
 make install BINDIR=$PREFIX/bin MANDIR=$PREFIX/man/man8
