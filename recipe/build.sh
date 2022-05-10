79a43bb8a0935359ccb10945aa520126f45260bf
Simon Bray <32272674+simonbray@users.noreply.github.com>
2022-05-10 16:31:21 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,6 @@ if [[ $(uname) == "Darwin" ]]; then
     export ARCH="MACOSXX86_64"
 fi
 
-#ln -s ${PREFIX}/lib/* plugins/${ARCH}/molfile
-
 export LINKER="LD_LIBRARY_PATH=plugins/${ARCH}/molfile/ ${CC}"
 
 make LINKER="${LINKER}" ARCH=${ARCH} CC=${CC} CCQHULL=${CC}
