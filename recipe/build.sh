12c32dd596fc1eb9060d1222343f52499367fd46
Simon Bray <32272674+simonbray@users.noreply.github.com>
2026-03-09 14:48:03 +0000

    add cflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,7 @@ if [[ $(uname) == "Darwin" ]]; then
 fi
 
 export LINKER="LD_LIBRARY_PATH=plugins/${ARCH}/molfile/ ${CC}"
+export CFLAGS="-Wno-error=incompatible-pointer-types"
 
 make LINKER="${LINKER}" ARCH=${ARCH} CC=${CC} CCQHULL=${CC}
 make install BINDIR=$PREFIX/bin MANDIR=$PREFIX/man/man8 LIBDIR=$PREFIX/lib
